6cf1852a4ea4a89f17e487edcd4062e2b7c80f63
LeonAngelit <81989897+LeonAngelit@users.noreply.github.com>
2022-11-17 10:59:46 +0100

    changed jakarta for javax

--- springapp/src/main/java/com/java/assessment/springapp/repositories/UserRepository.java
+++ springapp/src/main/java/com/java/assessment/springapp/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import com.java.assessment.springapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public interface UserRepository extends JpaRepository<User, Integer> {
 
