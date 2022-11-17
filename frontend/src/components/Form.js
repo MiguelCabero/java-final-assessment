e868402f0f9fd8833b300fe218fca7352db25bad
LeonAngelit <81989897+LeonAngelit@users.noreply.github.com>
2022-11-17 12:00:44 +0100

    change SQL query

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
-function Form(){
-
+function Form() {
+	return (
+		<div className='login-form'>
+			<form>
+				<div className='input-container'>
+					<label htmlFor='email'>Email: </label>
+					<input
+						type='email'
+						placeholder='Email'
+						name='email'
+						ref={emailRef}
+					/>
+				</div>
+				<div className='input-container'>
+					<label htmlFor='password'>Password: </label>
+					<input
+						type='password'
+						placeholder='Password'
+						name='password'
+						ref={passwordRef}
+					/>
+				</div>
+				<div className='submit-container'>
+					<input
+						type='submit'
+						defaultValue='Accept'
+						onClick={request}
+					/>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default Form;
--- springapp/src/main/resources/data.sql
+++ springapp/src/main/resources/data.sql
@@ -1,7 +1,8 @@
-CREATE TABLE IF NOT EXISTS `spring_final_assessment`.`miguel_leon_assessment` (
-  `id` INT NULL AUTO_INCREMENT,
-  `firstName` VARCHAR(25) NULL,
-  `lastName` VARCHAR(25) NULL,
-  `phoneNumber` VARCHAR(20) NULL,
-  `email` VARCHAR(25) NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS `miguel_leon_assessment` (
+  `id` int NOT NULL AUTO_INCREMENT,
+   `first_name` varchar(25) DEFAULT NULL,
+    `last_name` varchar(25) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `phone_number` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
